b4dd25f3f0471a4d7cbd110a4a39a62ea0790a07
annlaure02 <alaure.g02@gmail.com>
2022-12-02 01:39:34 -0800

    header file

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+/* putchar*/
+int _putchar(char c);
+
+/** functions_str */
+char *_strdup(char *str);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
+#endif
