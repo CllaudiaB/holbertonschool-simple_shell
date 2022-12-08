156f1359aa767d8bfd11938ab4d33d94342195a4
annlaure02 <alaure.g02@gmail.com>
2022-12-08 00:51:42 -0800

    add strcmp in functions_str and prototype in header

--- functions_str.c
+++ functions_str.c
@@ -101,13 +101,22 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+   * _strcmp - compares two strings exactly like strcmp
+    *
+     * @s1: parameter of the function
+      * @s2: parameter of the function
+       *
+        * Return: dest
+	 */
+
+int _strcmp(char *s1, char *s2)
 {
-	return (write(1, &c, 1));
+	int i = 0;
+
+	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
+		i++;
+	if (s1[i] == s2[i])
+		return (0);
+	else
+		return (s1[i] - s2[i]);
 }
--- shell.h
+++ shell.h
@@ -16,13 +16,15 @@ char *readline(void);
 char **split_string(char *str);
 int exec_cmd(char **argv);
 char *_getenv(const char *name);
-char *_path(char *str);
-
+char *_path(char *getcmd);
+char **split_env(char *path);
+void _printenv(void);
 
 /** functions_str */
 char *_strdup(char *str);
 char *_strcat(char *dest, char *src);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 
 #endif
